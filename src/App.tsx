53dfa121a4c6fe990d29e4b3759cdfecd7a1e147
iznomag <iznomag@gmail.com>
2020-08-15 23:35:41 +0200

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/main.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="max-w-sm rounded overflow-hidden shadow-lg">Hola</div>;
 }
 
 export default App;
